ea553331c8b97fb29836f25848f96d37c16eda11
adogins <124842665+adogins@users.noreply.github.com>
2026-01-17 22:33:10 -0500

    modified MYSQL Schema

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE files (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    file_path TEXT NOT NULL,
+    file_path VARCHAR(2048) NOT NULL,
     file_hash CHAR(64) NOT NULL,
     last_modified DATETIME NOT NULL,
     last_backup DATETIME NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE files (
 CREATE TABLE file_versions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     file_id INT NOT NULL,
-    backup_path TEXT NOT NULL,
+    backup_path VARCHAR(2048) NOT NULL,
     file_hash CHAR(64) NOT NULL,
     backed_up_at DATETIME NOT NULL,
     FOREIGN KEY (file_id) REFERENCES files(id)
